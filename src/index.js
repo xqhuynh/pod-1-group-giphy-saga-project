7b2bd10aac8eddafcc5000d5402bf513f03e5a01
Savon Huynh <xqhuynh@gmail.com>
2022-06-23 10:01:34 -0500

    Install dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+// import saga middleware from redux-saga
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import logger from 'redux-logger';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// watchsaga function*
+
+// Create store
+const store = createStore(
+    combineReducers({
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+)
+
+// Saga middleware
+const sagaMiddleware = createSagaMiddleware;
+
+ReactDOM.render
+    (
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById('root')
+    );
