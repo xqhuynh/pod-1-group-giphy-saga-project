886c18056e02bd352a7baefae25fba00fc11c4c4
Joey Lundquist <joeylundquist91@gmail.com>
2022-06-23 20:04:20 -0500

    added the rendering for the gifs

--- src/components/FavoriteListItems/FavoriteListItems.jsx
+++ src/components/FavoriteListItems/FavoriteListItems.jsx
@@ -4,8 +4,8 @@ const FavoriteListItems = ({ fav }) => {
 
 
     return (
-        <>
-            <li><img src={fav.data.data.images.url} /> 
+        <>   hi
+            <li><img src={fav.images.original.url} /> 
                 <select name="fav-gifs" id="fav-gifs">
                     <option value="1">Funny</option>
                     <option value="2">Cohort</option>
--- src/index.js
+++ src/index.js
@@ -5,15 +5,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 // import saga middleware from redux-saga
 import createSagaMiddleware from 'redux-saga';
-// import { takeEvery, put } from 'redux-saga/effects';
+import { takeEvery, put } from 'redux-saga/effects';
 import logger from 'redux-logger';
-// import axios from 'axios';
+import axios from 'axios';
 
-// watchersaga function*, will watch for actions
-
-function* watcherSaga() {
-
-}
 
 
 // Reducer that holds our results
@@ -26,9 +21,33 @@ const search = (state = {}, action) => {
 
 // Reducer that holds favorites
 const favorites = (state = {}, action) => {
-
+    switch(action.type){
+        case 'SET_FAVORITES':
+            return action.payload
+    }
     return state;
 }
+
+function* getFavoriteGifs(action) {
+    let res;
+    try{
+        res = yield axios.get('/api/favorite')
+    }
+    catch{
+        console.log('Failed to get Gifs', error)
+    }
+    yield put({
+        type:'SET_FAVORITES',
+        payload: res.data
+    })
+}
+
+
+// watchersaga function*, will watch for actions
+function* watcherSaga() {
+    yield takeEvery('GET_FAVORITE_GIFS', getFavoriteGifs)
+}
+
 // Saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
